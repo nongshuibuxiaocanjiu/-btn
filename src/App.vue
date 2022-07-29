add47a07d84ff09a8c47d1975bef3df84eabf8c3
jin <2293748571@qq.com>
2022-07-29 22:35:22 +0800

    完成btn

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <!-- type: primary:粉色，success:绿色-->
+    <!-- round 圆型 -->
+    <!-- disabled 禁用-->
+    <gButton type="primary" btnName="成功按钮" round @click="getType" disabled
+></gButton>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import gButton from './components/changeBtn.vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {};
+  },
+  components:{
+    gButton
+  },
+
+  created() {},
+
+  methods: {
+    getType() {
+      console.log(11);
+    }
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style scoped lang="less"></style>
---
+++ src/components/changeBtn.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <button
+      class="g-button"
+      :class="[
+        `g-btn-${type}`,
+        {
+          'is-round': round,
+          'is-disabled': disabled,
+        },
+      ]"
+      @click="btnclick"
+      :disabled="disabled"
+    >
+      {{ btnName }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      default: "default",
+    },
+    btnName: {
+      type: String,
+      default: "默认按钮",
+    },
+    round: {
+      type: Boolean,
+      default: false,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    btnclick(e) {
+      this.$emit("click", e);
+    },
+  },
+};
+</script>
+
+<style scoped lang="less">
+.g-btn-primary {
+  background-color: pink;
+  width: 100px;
+  height: 40px;
+  border: 1px solid #fff;
+  &:hover {
+    background: pink;
+    background-color: pink;
+    color: #fff;
+  }
+}
+.g-btn-success {
+  background-color: #71c216;
+  width: 100px;
+  height: 40px;
+  border: 1px solid #ccc;
+  &:hover {
+    background: #85ce61;
+    background-color: #85ce61;
+    color: #fff;
+  }
+}
+.is-round {
+  border-radius: 15px;
+  padding: 2px 3px;
+}
+// 意思是只要类名中包括g-icon就调用该样式
+[class*="g-icon"] {
+  font-family: "iconfont" !important;
+  font-size: 16px;
+  font-style: normal;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+// disabled样式
+.g-btn.is-disabled {
+  cursor: no-drop;
+}
+</style>
